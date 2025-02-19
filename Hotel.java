2ab222b8dd8fb9bd69e2135e51aab57edd01e210
Jananiboominathan <jananiboominathan2005@gmail.com>
2025-02-19 20:59:10 +0530

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,13 @@
+public class Hotel {
+    public void hotelDetails() {
+        String hotelName = "JB";
+        String hotelLocation = "Chennai";
+        System.out.println("WELCOME TO JB HOTEL");
+        System.out.println("Hotel name: " + hotelName);
+        System.out.println("Hotel location" + hotelLocation);
+        
+    }
+}
+
+    
+
